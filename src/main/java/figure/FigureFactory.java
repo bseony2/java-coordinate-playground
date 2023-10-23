872a4d47a0f5892993f1ceb3c78d042fbc9c4132
bseony2 <bseony2@gmail.com>
2023-10-23 23:47:26 +0900

    refactor : 예외 처리 및 생성자 추가

--- src/main/java/figure/FigureFactory.java
+++ src/main/java/figure/FigureFactory.java
@@ -1,14 +1,28 @@
 package figure;
 
+import coordinate.Point;
+
+import java.util.List;
+
 public class FigureFactory {
 
+    public static Figure generateFigure(List<Point> pointList) {
+        return generateFigure(new Points(pointList));
+    }
     public static Figure generateFigure(Points points) {
+        validate(points);
         if(FigureEnum.isLine(points)) {
             return generateLine(points);
         } else if (FigureEnum.isTriangle(points)) {
             return generateTriangle(points);
         }
         return generateSquare(points);
+    }
+
+    private static void validate(Points points) {
+        if(points.size() < 1 || points.size() > 4) {
+            throw new IllegalArgumentException("선분, 삼각형, 사각형만 입력이 가능합니다.");
+        }
     }
 
     private static Figure generateLine(Points points) {
